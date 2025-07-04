c8e2a94f01d565a67d476fceff739d6323606291
Zafar <zafarbekismailoff06@gmail.com>
2025-07-04 21:40:34 +0500

    Done

--- src/main.ts
+++ src/main.ts
@@ -1,1 +1,15 @@
-console.log(`Hello World`)
+// src/main.ts
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap';
+
+
+const btn = document.querySelector(".btn") as HTMLButtonElement;
+const photo = document.querySelector(".photo") as HTMLImageElement;
+
+function randomNumber(){
+    return Math.floor(Math.random() * 8) + 1;
+}
+
+btn.onclick = () => {
+    photo.src = `src/photo/photo-${randomNumber()}.jpg`;
+};
