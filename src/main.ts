25f4ab00d5b87093c5aedc18ca9f53198dd69c9d
Zafar <zafarbekismailoff06@gmail.com>
2025-07-04 22:43:14 +0500

    random-photo-v2

--- src/main.ts
+++ src/main.ts
@@ -1,15 +1,31 @@
-// src/main.ts
+
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap';
 
 
 const btn = document.querySelector(".btn") as HTMLButtonElement;
 const photo = document.querySelector(".photo") as HTMLImageElement;
+let intervalId: number | null = null;
 
 function randomNumber(){
     return Math.floor(Math.random() * 8) + 1;
 }
 
-btn.onclick = () => {
-    photo.src = `src/photo/photo-${randomNumber()}.jpg`;
-};
+btn.addEventListener("click", () => {
+   
+    if(btn.innerText === "Start") {
+        btn.innerText = "Stop";
+        intervalId = setInterval(() => {
+            const random = randomNumber();
+
+            photo.src = `src/photo/photo-${random}.jpg`;
+        }, 1500);
+    } else if(btn.innerText === "Stop") {
+        btn.innerText = "Start";
+        if (intervalId !== null) {
+            clearInterval(intervalId);
+            intervalId = null;
+        }
+        photo.src = "src/photo/photo-1.jpg";
+    }
+});
