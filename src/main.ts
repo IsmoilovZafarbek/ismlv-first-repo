b3e4f12e778de38fdbfd5f38f09dfdb1a538f5aa
Zafar <zafarbekismailoff06@gmail.com>
2025-07-17 09:46:25 +0500

    deploy test

--- src/main.ts
+++ src/main.ts
@@ -1,58 +1,16 @@
+const box = document.querySelector('.box') as HTMLDivElement;
+const button = document.querySelector('.btn') as HTMLButtonElement;
 
-const previousBtn = document.querySelector(".previous-btn") as HTMLButtonElement;
-const nextBtn = document.querySelector(".next-btn") as HTMLButtonElement;
-const photo = document.querySelector(".photo") as HTMLImageElement;
-const startBtn = document.querySelector(".btn-animation") as HTMLButtonElement;
-
-const images = [
-    "src/photo/photo-1.jpg",
-    "src/photo/photo-2.jpg",
-    "src/photo/photo-3.jpg",
-    "src/photo/photo-4.jpg",
-    "src/photo/photo-5.jpg",
-    "src/photo/photo-6.jpg",
-    "src/photo/photo-7.jpg",
-    "src/photo/photo-8.jpg"
-]
-
-let currentIdx = 0;
-function updateImage() {
-    photo.src = images[currentIdx];
-  }
-  
-  nextBtn.addEventListener("click", () => {
-    currentIdx = (currentIdx + 1) % images.length;
-    updateImage();
-  });
-  
-  previousBtn.addEventListener("click", () => {
-    currentIdx = (currentIdx - 1 + images.length) % images.length;
-    updateImage();
-  });
-
-
-  let intervalId: number | null = null;
-
-function randomNumber(){
-    return Math.floor(Math.random() * 8) + 1;
-}
-
-startBtn.addEventListener("click", () => {
-    
-   
-    if(startBtn.innerText === "Start Animation") {
-        startBtn.innerText = "Stop Animation";
-        intervalId = setInterval(() => {
-            const random = randomNumber();
-
-            photo.src = `src/photo/photo-${random}.jpg`;
-        }, 1500);
-    } else if(startBtn.innerText === "Stop Animation") {
-        startBtn.innerText = "Start Animation";
-        if (intervalId !== null) {
-            clearInterval(intervalId);
-            intervalId = null;
-        }
-        photo.src = "src/photo/photo-1.jpg";
+function generateHexColor(){
+    let hexColor = '#';
+    const hexValues = '0123456789abcdef';
+    for (let i = 0; i < 6; i++) {
+        hexColor += hexValues[Math.floor(Math.random() * hexValues.length)];
     }
-});
+    return hexColor;
+};
+button.onclick = () => {
+    const randomColor = generateHexColor();
+    box.style.backgroundColor = randomColor;
+    box.textContent = randomColor;
+}
